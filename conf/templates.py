1df7f4e1801564a231cc3d0b308da4729533f6f1
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-10-19 11:21:09 +0200

    Ajout de couleurs pour les graphes (#830)
    Change-Id: I2cf33548e84ef4a93ceccdce7b3ce7d6844142f1
    Refs: #830
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/336
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- conf/templates.py
+++ conf/templates.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2006-2011 CS-SI
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
+from copy import deepcopy
+
 templates = {}
 
 templates["lines"] = {
@@ -11,28 +13,21 @@ templates["lines"] = {
         { "type": "LINE1", "color": "#FF5500" },
         { "type": "LINE1", "color": "#FF9900" },
         { "type": "LINE1", "color": "#FFDD00" },
-        { "type": "LINE1", "color": "#CCFF66" },
         { "type": "LINE1", "color": "#86ED00" },
+        { "type": "LINE1", "color": "#00FF55" },
+        { "type": "LINE1", "color": "#00FFDD" },
         { "type": "LINE1", "color": "#0086ED" },
+        { "type": "LINE1", "color": "#5500FF" },
+        { "type": "LINE1", "color": "#9900FF" },
+        { "type": "LINE1", "color": "#DD00FF" },
         { "type": "LINE1", "color": "#6700ED" },
+        { "type": "LINE1", "color": "#EDDD00" },
     ],
 }
-templates["lines-from-zero"] = {
-    "tabs": [ 'AVERAGE', 'MIN', 'MAX', 'LAST'],
-    # Each DS will be assigned a different drawing method, in this order
-    "draw": [
-        { "type": "LINE1", "color": "#EE0088" },
-        { "type": "LINE1", "color": "#FF5500" },
-        { "type": "LINE1", "color": "#FF9900" },
-        { "type": "LINE1", "color": "#FFDD00" },
-        { "type": "LINE1", "color": "#CCFF66" },
-        { "type": "LINE1", "color": "#86ED00" },
-        { "type": "LINE1", "color": "#0086ED" },
-        { "type": "LINE1", "color": "#6700ED" },
-    ],
-    # the low limit of the graph is set to 0
-    "options": ['-l 0'],
-}
+
+templates["lines-from-zero"] = deepcopy(templates["lines"])
+# the low limit of the graph is set to 0
+templates["lines-from-zero"]["options"] = ['-l 0']
 
 templates["areas"] = {
     "tabs": [ 'AVERAGE', 'MIN', 'MAX', 'LAST'],
@@ -42,29 +37,21 @@ templates["areas"] = {
         { "type": "AREA", "color": "#FF5500" },
         { "type": "AREA", "color": "#FF9900" },
         { "type": "AREA", "color": "#FFDD00" },
-        { "type": "AREA", "color": "#CCFF66" },
         { "type": "AREA", "color": "#86ED00" },
+        { "type": "AREA", "color": "#00FF55" },
+        { "type": "AREA", "color": "#00FFDD" },
         { "type": "AREA", "color": "#0086ED" },
+        { "type": "AREA", "color": "#5500FF" },
+        { "type": "AREA", "color": "#9900FF" },
+        { "type": "AREA", "color": "#DD00FF" },
         { "type": "AREA", "color": "#6700ED" },
+        { "type": "AREA", "color": "#EDDD00" },
     ],
 }
 
-templates["areas-from-zero"] = {
-    "tabs": [ 'AVERAGE', 'MIN', 'MAX', 'LAST'],
-    # Each DS will be assigned a different drawing method, in this order
-    "draw": [
-        { "type": "AREA", "color": "#EE0088" },
-        { "type": "AREA", "color": "#FF5500" },
-        { "type": "AREA", "color": "#FF9900" },
-        { "type": "AREA", "color": "#FFDD00" },
-        { "type": "AREA", "color": "#CCFF66" },
-        { "type": "AREA", "color": "#86ED00" },
-        { "type": "AREA", "color": "#0086ED" },
-        { "type": "AREA", "color": "#6700ED" },
-    ],
-    # the low limit of the graph is set to 0
-    "options": ['-l 0'],
-}
+templates["areas-from-zero"] = deepcopy(templates["areas"])
+# the low limit of the graph is set to 0
+templates["areas-from-zero"]["options"] = ['-l 0']
 
 templates["area-line"] = {
     "tabs": [ 'AVERAGE', 'MIN', 'MAX', 'LAST'],
@@ -82,10 +69,15 @@ templates["stacks"] = {
         { "type": "AREA", "color": "#FF5500", "stack": True },
         { "type": "AREA", "color": "#FF9900", "stack": True },
         { "type": "AREA", "color": "#FFDD00", "stack": True },
-        { "type": "AREA", "color": "#CCFF66", "stack": True },
         { "type": "AREA", "color": "#86ED00", "stack": True },
+        { "type": "AREA", "color": "#00FF55", "stack": True },
+        { "type": "AREA", "color": "#00FFDD", "stack": True },
         { "type": "AREA", "color": "#0086ED", "stack": True },
+        { "type": "AREA", "color": "#5500FF", "stack": True },
+        { "type": "AREA", "color": "#9900FF", "stack": True },
+        { "type": "AREA", "color": "#DD00FF", "stack": True },
         { "type": "AREA", "color": "#6700ED", "stack": True },
+        { "type": "AREA", "color": "#EDDD00", "stack": True },
     ],
 }
 
