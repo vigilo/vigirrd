192a0ce5947458fa9a220e3eca6926221dabcd54
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-11-04 13:25:29 +0100

    MAJ des versions
    Utilisation de la commande :
    `vigilo-dev-update-versions -d . -s .../r2.16`
    
    Change-Id: Ia76550736ee5d82b4bfbf210cfb40362a7633151

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ sysconfdir = os.getenv("SYSCONFDIR", "/etc")
 
 setup(
     name='vigilo-vigirrd',
-    version='2.0.6',
+    version='2.0.7',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
