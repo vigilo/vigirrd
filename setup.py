54d5cd9f87a0053d9ba0a553e83a0dc67db51bd1
Francois POIROTTE <francois.poirotte@c-s.fr>
2012-04-20 17:24:50 +0200

    Bump des versions (suite livraison un client r2.20).
    Change-Id: I8134b7c1c6828ac2c9cfdc773cdffb4fa21a98ef
    BuildBot: no-build
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/676
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    Tested-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ sysconfdir = os.getenv("SYSCONFDIR", "/etc")
 
 setup(
     name='vigilo-vigirrd',
-    version='2.0.15',
+    version='2.0.16',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
