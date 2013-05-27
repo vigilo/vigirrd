7bd53ba2608797672effa718e55f363c70d4d6e1
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2013-05-27 11:39:10 +0200

    Bump des versions
    Change-Id: I0fa8cd7560b3bb208e948ab3b5d7c69a82930f85
    BuildBot: no-build
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1156
    Reviewed-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>
    Tested-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ sysconfdir = os.getenv("SYSCONFDIR", "/etc")
 
 setup(
     name='vigilo-vigirrd',
-    version='3.1',
+    version='3.2',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
