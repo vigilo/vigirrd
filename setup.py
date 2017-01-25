40d5bdf32f854cb8c8127f587e658bee3b27623b
Yves OUATTARA <yves.ouattara@c-s.fr>
2017-01-25 17:08:25 +0100

    Bump version: passage en 4.0
    Change-Id: I3636cf85119954866dc5f3d3e0028bff28cd4d42
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/2296
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ cronext = os.getenv("CRONEXT", ".cron")
 
 setup(
     name='vigilo-vigirrd',
-    version='3.6.0',
+    version='4.0',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
