80ad32e3f9692cb0962110ff2417b04b7bddc5eb
Francois POIROTTE <francois.poirotte@c-s.fr>
2017-06-21 15:53:02 +0200

    Migration des tests vers gearbox
    Change-Id: Id7bea4749ed31f5dee42e472b8f538c224d3e10a

--- vigirrd/tests/__init__.py
+++ vigirrd/tests/__init__.py
@@ -4,56 +4,24 @@
 
 """Unit and functional test suite for vigirrd."""
 
-import os
-import sys
 import platform
-
+import os.path
 import unittest
-from tg import config
+
 from paste.deploy import loadapp
-from paste.script.appinstall import SetupCommand
 from webtest import TestApp
-from nose.tools import eq_
 
 from vigirrd.model import metadata, DBSession
 
-__all__ = ['setup_db', 'teardown_db', 'TestController']
-
-#def setup_db():
-#    """Method used to build a database"""
-#    print "Creating model"
-#    engine = config['pylons.app_globals'].sa_engine
-#    metadata.create_all(engine)
-#
-#def teardown_db():
-#    """Method used to destroy a database"""
-#    print "Destroying model"
-#    engine = config['pylons.app_globals'].sa_engine
-#    metadata.drop_all(engine)
+__all__ = ['TestController']
 
 class TestController(unittest.TestCase):
-    """
-    Base functional test case for the controllers.
-
-    The vigirrd application instance (``self.app``) set up in this test
-    case (and descendants) has authentication disabled, so that developers can
-    test the protected areas independently of the :mod:`repoze.who` plugins
-    used initially. This way, authentication can be tested once and separately.
-
-    Check vigirrd.tests.functional.test_authentication for the repoze.who
-    integration tests.
-
-    This is the officially supported way to test protected areas with
-    repoze.who-testutil (http://code.gustavonarea.net/repoze.who-testutil/).
-
-    """
-
     application_under_test = 'main_without_authn'
 
     def setUp(self):
         """Method called by nose before running each test"""
         # Loading the application:
-        conf_dir = config.here
+        conf_dir = "."
 
         # Charge "test32.ini" pour les archis 32 bits
         # et "test64.ini" pour les archis 64 bits.
@@ -63,11 +31,3 @@ class TestController(unittest.TestCase):
                                             self.application_under_test),
                           relative_to=conf_dir)
         self.app = TestApp(wsgiapp)
-        ## Setting it up:
-        #cmd = SetupCommand('setup-app')
-        #cmd.run([config_file])
-
-    #def tearDown(self):
-    #    """Method called by nose after running each test"""
-    #    engine = config['pylons.app_globals'].sa_engine
-    #    metadata.drop_all(engine)
