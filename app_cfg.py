47bfa68616af647b6979a13f4ea445cde57035e0
Francois POIROTTE <francois.poirotte@c-s.fr>
2019-02-11 15:07:52 +0100

    Mise à jour des copyrights
    Change-Id: I471e22a6b59d98c890063c5e2f18da07d9ad4d30

--- app_cfg.py
+++ app_cfg.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
-# Copyright (C) 2006-2018 CS-SI
+# Copyright (C) 2006-2019 CS-SI
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 """
