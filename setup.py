50bbbe8cb696f55bcb41e51de80b8c9fb17cda7b
Francois POIROTTE <francois.poirotte@c-s.fr>
2019-08-16 11:16:02 +0200

    Bump v5.1.0b1
    Change-Id: Ib5c7dba91905b32d6e120423d2a347aec82fc6fa
    BuildBot: no-build

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ cronext = os.getenv("CRONEXT", ".cron")
 
 setup(
     name='vigilo-vigirrd',
-    version='5.1.0a3',
+    version='5.1.0b1',
     author='Vigilo Team',
     author_email='contact.vigilo@c-s.fr',
     url='https://www.vigilo-nms.com/',
