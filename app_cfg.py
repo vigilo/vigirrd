f26844429db096a0ba6e4ef4352591eba730a758
Francois POIROTTE <francois.poirotte@c-s.fr>
2020-06-30 17:09:09 +0200

    [#2026] Changement de dénomination sociale
    Change-Id: I5348d17ecc3cdb78232d7847f2b32c4722bf0710

--- app_cfg.py
+++ app_cfg.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
-# Copyright (C) 2006-2020 CS GROUP – France
+# Copyright (C) 2006-2020 CS GROUP - France
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 """
--- vigirrd/tests/testdata/conf/test.py
+++ vigirrd/tests/testdata/conf/test.py
@@ -1,5 +1,5 @@
 # vim: fileencoding=utf-8 sw=4 ts=4 et ai
-# Copyright (C) 2011-2020 CS GROUP – France
+# Copyright (C) 2011-2020 CS GROUP - France
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 templates = {"lines":
